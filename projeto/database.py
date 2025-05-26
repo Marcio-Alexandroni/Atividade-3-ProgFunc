70ba8c086dcfbbb5d88fe1d309d56b6c22d58dcf
impauloc <paulo.carmo@acad.espm.br>
2025-05-26 12:25:28 -0300

    Revert "Banco criado, API funcionando, porém não está processando corretamente ainda cada transação"
    This reverts commit 530a53ff276e5f1d66e414bacf52378e8e3baa2a.

--- projeto/database.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
-
-# ajuste user, senha e host se necessário
-DATABASE_URL = "mysql+pymysql://root:root@localhost:3306/ativ3"
-
-engine = create_engine(DATABASE_URL, echo=True)
-SessionLocal = sessionmaker(bind=engine, expire_on_commit=False)
-Base = declarative_base()
